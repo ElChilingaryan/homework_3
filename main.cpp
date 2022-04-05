58daabb3be490690f57e800bdfb4fd3c3d87ce2f
ElChilingaryan <ellchilingaryan@gmail.com>
2022-04-05 11:48:43 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <thread_pool.h>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <queue>
+#include <stdexcept>
+#include <ctime>
+#include <cstdlib>
+
+
+
+int main()
+{
+ using namespace std;
+ srand(time(NULL));
+ mutex cout_guard;
+ 
+ cout << this_thread::get_id() << endl;
+ 
+ thread_pool tp;
+ for(auto i = 1; i <= 10; i++)
+  tp.do_work([&, i = 1]()
+  {
+   {
+    unique_lock guard(cout_guard);
+    cout << i << endl;
+   }
+   this_thread::sleep_for(chrono::milliseconds(rand()%1000));
+  });
+}                          
+                  
+       
